c4616194dba0cc003756ae93eaa66f1944a16167
ronymep <ronymep@gmail.com>
2024-05-11 17:42:26 +0530

    added dummy data

---
+++ mock/DummyData.ts
@@ -0,0 +1,19 @@
+// DummyData.js
+
+export const featuresData = [
+    {
+        title: "Age Predictor",
+        description: "Predicts the age based on the given name.",
+        icon: "age_icon.png",
+    },
+    {
+        title: "Location Predictor",
+        description: "Predicts the location based on the given name.",
+        icon: "location_icon.png", 
+    },
+    {
+        title: "Gender Predictor",
+        description: "Predicts the gender based on the given name.",
+        icon: "gender_icon.png",
+    },
+];
